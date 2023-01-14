cf11925b40ba4ba72fa94b68bdbe7f5647b1fe30
breadodin <110830686+breadodin@users.noreply.github.com>
2023-01-14 17:51:40 +0700

    Add files via upload

---
+++ src/Tests/s21_math_tests.c
@@ -0,0 +1,33 @@
+#include "s21_math_test.h"
+
+int main() {
+    static int count = 1;
+    Suite *cases[] = {
+        abs_tests(), acos_tests(), asin_tests(),
+        atan_tests(), ceil_tests(), cos_tests(), 
+        exp_tests(), fabs_tests(), floor_tests(), 
+        fmod_tests(), log_tests(), pow_tests(), 
+        sin_tests(), sqrt_tests(), tan_tests(),
+        NULL};
+    printf("\n");
+    for (Suite **currentTest = cases; *currentTest != NULL; currentTest++) {
+        printf("Current Test: %d\n", count);
+        count++;
+        SRunner *runner = srunner_create(*currentTest);
+        srunner_set_fork_status(runner, CK_NOFORK);
+        srunner_run_all(runner, CK_NORMAL);
+        srunner_free(runner);
+        printf("\n");
+    }
+    return 0;
+}
+
+int CastToStandart(int casted) {
+    int result = 0;
+    if (casted > 0) {
+        result = 1;
+    } else if (casted < 0) {
+        result = -1;
+    }
+    return result;
+}
